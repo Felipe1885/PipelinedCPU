95eba9804bccc217c1544fec8229c84c11d9be6d
Felipe1885 <felipe.leonardi2005@gmail.com>
2025-11-25 16:20:33 -0300

    Memória de Instruções arrumada, com os NOPs certos

--- InstructionMemory.vhd
+++ InstructionMemory.vhd
@@ -3,85 +3,82 @@ USE ieee.std_logic_1164.all ;
 use IEEE.NUMERIC_STD.ALL;
 USE work.package_components.all;
 
---Entidade da Memoriua
-ENTITY Memory IS
+--Entidade da Memória
+ENTITY InstructionMemory IS
 	PORT ( Address : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
            Instruction : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
-        );
-END Memory;
+    );
+END InstructionMemory;
 
-ARCHITECTURE Behavior OF Memory IS
+ARCHITECTURE Behavior OF InstructionMemory IS
 
 TYPE MemArray IS ARRAY (65535 DOWNTO 0) OF STD_LOGIC_VECTOR (7 DOWNTO 0);
 SIGNAL Mem : MemArray := (        
 
-        -- Endereço 0x0000 (0): LW $r1, 5($r0) 
+        -- Endereço 0x0000 (0): LW $r1, 2($r0) 
         0 => b"00100000",  
-        1 => b"00010101",   
+        1 => b"00010010",   
 
-        -- LW $r2, 10($r0)
-        2  => b"00100000",
-        3  => b"00101010",
-
+        -- 3 NOPs do load
+        2 => b"00000000", 
+        3 => b"00000000", 
         --NOP
         4 => b"00000000", 
         5 => b"00000000", 
-
         --NOP
         6 => b"00000000", 
-        7 => b"00000000", 
+        7 => b"00000000",
 
-        -- ADD $r3, $r1, $r2 
+
+        -- ADD $r2, $r1, $r1 
         8  => b"01100001",
-        9  => b"00100011",
-
-        --NOP
+        9  => b"00010010",
+        
+        --3 NOPs do add
         10 => b"00000000", 
         11 => b"00000000", 
-
         --NOP
         12 => b"00000000", 
         13 => b"00000000", 
-
-        -- SUB $r4, $r3, $r1
-        14  => b"01100011",
-        15  => b"00010100",
-
         --NOP
-        16 => b"00000000", 
-        17 => b"00000000", 
+        14 => b"00000000", 
+        15 => b"00000000", 
 
-        --NOP
+
+        -- SUB $r3, $r2, $r1
+        16  => b"01100010",
+        17  => b"00010011",
+
+        --3 NOPs do sub
         18 => b"00000000", 
         19 => b"00000000", 
-
-        -- BEQ $r4, $r2, 1
-        20 => b"10000010",
-        21 => b"01000001",   -- Offset = 1
-
+        --NOP
+        20 => b"00000000", 
+        21 => b"00000000", 
         --NOP
         22 => b"00000000", 
-        23 => b"00000000", 
+        23 => b"00000000",
 
-        --NOP
-        24 => b"00000000", 
-        25 => b"00000000", 
-    
-        --NOP
+
+        -- BEQ $r3, $r2, 3
+        24 => b"10000011",
+        25 => b"00010011",   -- Offset = 3
+
+        -- 3 NOPs do branch
         26 => b"00000000", 
         27 => b"00000000", 
-
-        -- ADD $r1, $r1, $r1
-        28 => b"01100001",
-        29 => b"00010001",
-
-        --NOP
+        -- NOP
+        28 => b"00000000", 
+        29 => b"00000000", 
+        -- NOP
         30 => b"00000000", 
         31 => b"00000000", 
-    
-        --NOP
-        32 => b"00000000", 
-        33 => b"00000000",
+
+
+        -- SW $r3, 50($r0)
+        32 => b"01000000",
+        33 => b"11010010",
+
 
         -- JMP 0
         34 => b"10100000",
@@ -92,7 +89,7 @@ SIGNAL Mem : MemArray := (
     
 SIGNAL IntegerAddrs : integer;
 
-	BEGIN
+BEGIN
 
     IntegerAddrs <= to_integer(unsigned(Address));
 
